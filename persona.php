71bb8435db44e7461c70a87cba4a01957e649749
profeweb <42413333+mitjanit@users.noreply.github.com>
2019-07-03 21:34:47 +0200

    Modal Trajectoria Arbit

--- persona.php
+++ persona.php
@@ -900,7 +900,7 @@ $nomTaula = "Persona";
 
   <?php include 'modals/modal_trajectoria_jugador_new.php'; ?>
   <?php include 'modals/modal_trajectoria_entrenador_new.php'; ?>
-  <?php //include 'modals/modal_trajectoria_arbit_new.php'; ?>
+  <?php include 'modals/modal_trajectoria_arbit_new.php'; ?>
   <?php //include 'modals/modal_trajectoria_directiu_new.php'; ?>
   <?php //include 'modals/modal_trajectoria_altre_new.php'; ?>
 
