2e480d15f9824aed4cf73b7c10765073f457cddc
profeweb <42413333+mitjanit@users.noreply.github.com>
2019-07-07 18:27:24 +0200

    Update editaCompeticio.php

--- db/editaCompeticio.php
+++ db/editaCompeticio.php
@@ -1,15 +1,4 @@
 <?php include("../common/mysessions.php"); ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="">
-  <meta name="author" content="">
-  <title>Admin - Museu Futbol Menorquí</title>
-</head>
-<body>
 <?php require_once('../connexions/connexio01.php'); ?>
 <?php 
 
@@ -24,16 +13,12 @@ mysqli_query($con01, $sql) or die(mysqli_error());
 
 
 // LOGs
-
 $user = $_SESSION['username'];
 $table = "Competicions";
 $sqlLog = "INSERT INTO accions_user (user, tipus, taula, temps) VALUES ('".$user."','U','".$table."', CURRENT_TIMESTAMP)";
 mysqli_query($con01, $sqlLog) or die(mysqli_error());
 
+header("Location: ../competicions.php?msgOk",TRUE, 302);
+exit;
 
 ?>
-<p>HOLLLLLLLLLLLLLLLLLLLaaaaa</p>
-<script>
-window.location.replace("../competicions.php");
-</script>
-</body>
