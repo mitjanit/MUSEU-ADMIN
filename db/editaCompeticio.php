5c174445bf7fda805f8ee4596ab984e18341c110
profeweb <42413333+mitjanit@users.noreply.github.com>
2019-07-07 18:24:20 +0200

    Update editaCompeticio.php

--- db/editaCompeticio.php
+++ db/editaCompeticio.php
@@ -12,7 +12,7 @@
 <body>
 <?php require_once('../connexions/connexio01.php'); ?>
 <?php 
-/*
+
 
 $id = $_REQUEST['id'];
 $nom = (addslashes($_REQUEST['nom']));
@@ -29,9 +29,7 @@ $table = "Competicions";
 $sqlLog = "INSERT INTO accions_user (user, tipus, taula, temps) VALUES ('".$user."','U','".$table."', CURRENT_TIMESTAMP)";
 mysqli_query($con01, $sqlLog) or die(mysqli_error());
 
-//header("Location: ../competicions.php?msgOk",TRUE, 302);
-//exit();
-*/
+
 ?>
 <p>HOLLLLLLLLLLLLLLLLLLLaaaaa</p>
 <script>
