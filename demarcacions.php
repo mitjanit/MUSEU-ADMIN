49f16cae9f14fc851591d5a7761aa1851f58a0b1
profeweb <42413333+mitjanit@users.noreply.github.com>
2019-07-08 14:18:49 +0200

    view role user

--- demarcacions.php
+++ demarcacions.php
@@ -42,12 +42,13 @@ $nomTaula = "Demarcacions";
             <div class="card-body">
               <div class="row" id="zona-botons">
                   <div class=" col-md-6">
-
+                    <?php if($_SESSION['role']=="A" || $_SESSION['role']=="E") { ?>
                       <div class="btn-group"  role="group">
                           <button type="button" class="btn btn-primary btn-new" data-toggle="modal" data-target="#myNewModal"><i class="fas fa-plus-circle"></i> Alta</button>
                           <button type="button" class="btn btn-primary btn-edit" data-toggle="modal" data-target="#myEditModal"><i class="fa fa-edit"></i> Edita</button>
                           <button type="button" class="btn btn-primary btn-delete" data-toggle="modal" data-target="#myDeleteModal"><i class="fas fa-minus-circle"></i> Elimina</button>
                       </div>
+                    <?php } ?>
                   </div>
                   <div class="col-md-6"></div>
               </div>
