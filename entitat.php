b9c97d8a5bb921c73b99af40ce87017ae0f3a6c4
profeweb <42413333+mitjanit@users.noreply.github.com>
2019-07-10 12:22:18 +0200

    Update entitat.php

--- entitat.php
+++ entitat.php
@@ -93,7 +93,7 @@
                                           <td>
                                               <?php 
 
-                                              $query_pais = "SELECT * FROM paisos WHERE id='".$row['paisos_id']."'";
+                                              $query_pais = "SELECT * FROM paisos WHERE id='".$row['pais_id']."'";
                                                   $rspais = mysqli_query($con01, $query_pais) or die("Error: ".mysqli_error($con01));
                                                   $rowpais = mysqli_fetch_array($rspais);
                                                   echo ($rowpais['nom']); 
