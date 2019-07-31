29955ad96571f4f0ce9dba66a0dda4404bdaf82e
profeweb <42413333+mitjanit@users.noreply.github.com>
2019-07-31 21:24:02 +0200

    Canvi Alta ENTITAT

--- db/altaEntitat.php
+++ db/altaEntitat.php
@@ -22,7 +22,7 @@ $bandera = $_REQUEST['bandera'];
 $altre = (addslashes($_REQUEST['altre']));
 $detalls2 = (addslashes($_REQUEST['detalls2']));
 
-$sql ="INSERT INTO clubs (nom, poblacions_id, paisos_id, provincia_id, inici, inici_detall, nom_esportiu, escut, himne, cd, insignia, camiseta, bufanda, bandera, altre, detalls2) VALUES ('".$nom."', '".$poble."', '".$pais."', '".$provincia."', '".$inici."', '".$detallsinici."', '".$nome."', '".$escut."', '".$himne."', '".$cd."', '".$insignia."', '".$camiseta."', '".$bufanda."', '".$bandera."', '".$altre."', '".$detalls2."')";
+$sql ="INSERT INTO clubs (nom, poblacions_id, pais_id, provincia_id, inici, inici_detall, nom_esportiu, escut, himne, cd, insignia, camiseta, bufanda, bandera, altre, detalls2) VALUES ('".$nom."', '".$poble."', '".$pais."', '".$provincia."', '".$inici."', '".$detallsinici."', '".$nome."', '".$escut."', '".$himne."', '".$cd."', '".$insignia."', '".$camiseta."', '".$bufanda."', '".$bandera."', '".$altre."', '".$detalls2."')";
 echo $sql."<br/>";
 mysqli_query($con01, $sql) or die(mysqli_error());
 
