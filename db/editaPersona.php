c937ed03f022cd6452dae86d4f8218e7c8b11aeb
profeweb <42413333+mitjanit@users.noreply.github.com>
2019-07-31 21:30:35 +0200

    Update editaPersona.php

--- db/editaPersona.php
+++ db/editaPersona.php
@@ -26,7 +26,7 @@ $poble = $_REQUEST['poblacio'];
 $provincia = $_REQUEST['provincia'];
 $pais = $_REQUEST['pais'];
 
-$sql ="UPDATE persones SET nom_esportiu =  '".$nome."', nom = '".$nom."', ap1 = '".$ap1."', ap2 = '".$ap2."', croniques = '".$croniques."', fotos = '".$fotos."', entrevistes = '".$entrevistes."', data_naixament =  '".$neix."', mort = '".$mort."', mort_detall = '".$detallsmort."', detalls = '".$detalls."', material =  '".$material."', ficha = '".$ficha."', dada1 = '".$dada1."', dada2 = '".$dada2."', dada3 = '".$dada3."', detalls2 = '".$detalls2."', dni = '".$dni."' WHERE id='".$id."'";
+$sql ="UPDATE persones SET nom_esportiu =  '".$nome."', nom = '".$nom."', ap1 = '".$ap1."', ap2 = '".$ap2."', croniques = '".$croniques."', fotos = '".$fotos."', entrevistes = '".$entrevistes."', data_naixament =  '".$neix."', mort = '".$mort."', mort_detall = '".$detallsmort."', detalls = '".$detalls."', material =  '".$material."', ficha = '".$ficha."', dada1 = '".$dada1."', dada2 = '".$dada2."', dada3 = '".$dada3."', detalls2 = '".$detalls2."', dni = '".$dni."', poblacions_id='".$poble."', provincia_id='".$provincia."', paisos_id='".$pais."' WHERE id='".$id."'";
 //echo $sql."<br/>";
 mysqli_query($con01, $sql) or die(mysqli_error());
 
@@ -37,7 +37,7 @@ $table = "Persona";
 $sqlLog = "INSERT INTO accions_user (user, tipus, taula, temps) VALUES ('".$user."','U','".$table."', CURRENT_TIMESTAMP)";
 mysqli_query($con01, $sqlLog) or die(mysqli_error());
 
-header("Location: ../persones.php?&msgOk",TRUE, 302);
-exit;
+//header("Location: ../persones.php?&msgOk",TRUE, 302);
+//exit;
 
 ?>
