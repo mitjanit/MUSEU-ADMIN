40d2c7561b5765f5ea59bbe0602064b62fd4f58a
profeweb <42413333+mitjanit@users.noreply.github.com>
2020-06-10 11:23:57 +0200

    Update relats.php

--- relats.php
+++ relats.php
@@ -44,7 +44,7 @@ $nomTaula = "Relats";
                   <div class=" col-md-6">
                     <?php if($_SESSION['role']=="A" || $_SESSION['role']=="E") { ?>
                       <div class="btn-group"  role="group">
-                          <button type="button" class="btn btn-primary btn-new" data-toggle="modal" data-target="#myNewModal"><i class="fas fa-plus-circle"></i> Alta</button>
+                          <button type="button" class="btn btn-primary btn-new" data-toggle="modal" data-target="#myNewModalRelat"><i class="fas fa-plus-circle"></i> Alta</button>
                           <button type="button" class="btn btn-primary btn-edit" data-toggle="modal" data-target="#myEditModal"><i class="fa fa-edit"></i> Edita</button>
                           <button type="button" class="btn btn-primary btn-delete" data-toggle="modal" data-target="#myDeleteModal"><i class="fas fa-minus-circle"></i> Elimina</button>
                       </div>
